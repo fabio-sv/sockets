37c9a2ed6482bfbb640d1dc26c555c9707706471
Fabio Sousa Vieira <95920140+fabio-sv@users.noreply.github.com>
2024-05-03 14:52:37 +0200

    Obfuscated complications to networking file

--- infrastructure/iam.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_role" "main" {
-  name                = "ServerlessChat"
-  assume_role_policy  = file("./policies/assume-role.json")
-  managed_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
-}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,40 +1,62 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+resource "aws_s3_bucket" "main" {
+  bucket = "my-super-duper-cool-bucket"
+
+  force_destroy = true
+}
+
+resource "aws_dynamodb_table" "connections" {
+  name           = "ServerlessChatConnectionTable"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "connectionId"
+
+  attribute {
+    name = "connectionId"
+    type = "S"
+  }
+}
+
+resource "aws_lambda_function" "connect" {
+  function_name = "connect"
+  role          = aws_iam_role.main.arn
+  runtime       = "python3.9"
+  handler       = "connect.handler"
+
+  environment {
+    variables = {
+      TABLE_NAME   = aws_dynamodb_table.connections.name
+      API_ENDPOINT = aws_apigatewayv2_stage.main.invoke_url
+    }
+  }
+
+  filename         = "./functions.zip"
+  source_code_hash = filebase64sha256("./functions.zip")
+}
+
+resource "aws_lambda_function" "disconnect" {
+  function_name = "disconnect"
+  role          = aws_iam_role.main.arn
+  runtime       = "python3.9"
+  handler       = "disconnect.handler"
+
+  environment {
+    variables = {
+      TABLE_NAME   = aws_dynamodb_table.connections.name
+      API_ENDPOINT = aws_apigatewayv2_stage.main.invoke_url
     }
   }
 }
 
-provider "aws" {
-  region = "eu-west-1"
+resource "aws_lambda_function" "send" {
+  function_name = "send"
+  role          = aws_iam_role.main.arn
+  runtime       = "python3.9"
+  handler       = "send.handler"
 
-  default_tags {
-    tags = {
-      "Application" = "ServerlessChat"
-      "Environment" = "Development"
-      "Purpose"     = "Live-Demo"
-      "CreatedBy"   = "Terraform"
-      "Owner"       = "fabiosv@bbd.co.za"
-      "DestroyBy"   = "2024-04-05"
+  environment {
+    variables = {
+      TABLE_NAME   = aws_dynamodb_table.connections.name
+      API_ENDPOINT = aws_apigatewayv2_stage.main.invoke_url
     }
   }
-}
-
-data "aws_caller_identity" "current" {}
-
-output "cloudfront_url" {
-  description = "URL of CloudFront distribution"
-  value       = aws_cloudfront_distribution.main.domain_name
-}
-
-output "api_url" {
-  description = "URL of API Gateway"
-  value       = aws_apigatewayv2_stage.main.invoke_url
-}
-
-output "bucket_name" {
-  description = "Name of S3 bucket"
-  value       = aws_s3_bucket.main.bucket
 }
